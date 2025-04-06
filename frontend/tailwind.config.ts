fde7bba84046f6e035f587af66949e65f4ee9746
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2025-04-06 15:15:41 +0200

    frontend: Slightly increase accordion target height

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -113,8 +113,8 @@ export default {
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        'accordion-down': 'accordion-down 0.3s ease-out',
+        'accordion-up': 'accordion-up 0.3s ease-out',
         'collapsible-down': 'collapsible-down 0.2s ease-in-out',
         'collapsible-up': 'collapsible-up 0.2s ease-in-out',
         'gradient-x': 'gradient-x 4s ease infinite',
