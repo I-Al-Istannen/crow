bd205594f276c7c9c00fb02669c3c5909813c562
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2025-04-19 23:21:19 +0200

    frontend: Set backend url via env file

--- frontend/src/data/fetching.ts
+++ frontend/src/data/fetching.ts
@@ -1,6 +1,6 @@
 import { useUserStore } from '@/stores/user.ts'
 
-export const BACKEND_URL = 'http://localhost:3000'
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 export class FetchError extends Error {
   readonly status: number
