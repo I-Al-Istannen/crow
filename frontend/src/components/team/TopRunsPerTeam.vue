da001127c5c56be5a4fdedaa44ee07d48878d8a6
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2025-05-07 00:21:39 +0200

    frontend: Switch to table for top teams when there are more than a few

--- frontend/src/components/team/TopRunsPerTeam.vue
+++ frontend/src/components/team/TopRunsPerTeam.vue
@@ -11,10 +11,36 @@
         :failure-reason="failureReason"
       />
       <div v-if="topTasks" class="space-y-2 -mt-2">
-        <div v-for="[team, task] in sortedTeams" :key="team" class="flex flex-col">
-          <span class="font-medium mb-1">{{ task.teamName }}:</span>
-          <FinishedTaskOverview :task="task" class="ml-2" />
-        </div>
+        <Table class="w-fit" v-if="topTasks.size > 3">
+          <TableHeader>
+            <TableHead class="text-center">Place</TableHead>
+            <TableHead class="text-center">Team</TableHead>
+            <TableHead class="text-center">Tests</TableHead>
+            <TableHead class="text-center">Time</TableHead>
+            <TableHead class="text-center">Task</TableHead>
+          </TableHeader>
+          <TableBody>
+            <TableRow v-for="([teamId, task], index) in sortedTeams" :key="teamId">
+              <TableCell class="py-0 text-center">{{ index + 1 }}</TableCell>
+              <TableCell class="py-0">{{ task.teamName }}</TableCell>
+              <TableCell class="py-0">
+                <TaskQuickOverview class="text-sm" :task="task" />
+              </TableCell>
+              <TableCell class="py-0">{{ task.info.start.toLocaleString() }}</TableCell>
+              <TableCell class="py-0">
+                <RouterLink :to="{ name: 'task-detail', params: { taskId: task.info.taskId } }">
+                  <Button variant="link">To the task</Button>
+                </RouterLink>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <template v-else>
+          <div v-for="[team, task] in sortedTeams" :key="team" class="flex flex-col">
+            <span class="font-medium mb-1">{{ task.teamName }}:</span>
+            <FinishedTaskOverview :task="task" class="ml-2" />
+          </div>
+        </template>
       </div>
     </CardContent>
   </Card>
@@ -22,9 +48,19 @@
 
 <script setup lang="ts">
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
 import DataLoadingExplanation from '@/components/DataLoadingExplanation.vue'
 import type { FinishedCompilerTaskSummary } from '@/types.ts'
 import FinishedTaskOverview from '@/components/task-overview/FinishedTaskOverview.vue'
+import TaskQuickOverview from '@/components/task-overview/TaskQuickOverview.vue'
 import { computed } from 'vue'
 import { queryTopTaskPerTeam } from '@/data/network.ts'
 import { vAutoAnimate } from '@formkit/auto-animate/vue'
@@ -36,7 +72,13 @@ const sortedTeams = computed(() => {
     return undefined
   }
   const result = [...topTasks.value.entries()]
-  result.sort((a, b) => finishedTests(b[1]) - finishedTests(a[1]))
+  result.sort((a, b) => {
+    const cmp = finishedTests(b[1]) - finishedTests(a[1])
+    if (cmp !== 0) {
+      return cmp
+    }
+    return a[0].localeCompare(b[0])
+  })
 
   return result
 })
